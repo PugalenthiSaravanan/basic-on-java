2359619a995094e1b1cc27ba8c3fa1ca207c490c
pugalpugal123 <pugalenthisaravanan70@gmail.com>
2026-08-13 20:32:17 +0530

    Added new code files

---
+++ operators/ArithmeticOperators.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperators{
+    public static void main (String [] args ){
+        int a = 10;
+        int b = 20;
+        //=== Arithmetic Operators ===
+
+        System.out.println("Addition :" + (a+b));
+
+         System.out.println("Subtraction :" + (a-b));
+        
+         System.out.println("Multiplication :" + (a*b));
+        
+         System.out.println("Division :" + (a/b));
+        
+         System.out.println("Modulus :" + (a%b));
+        
+         System.out.println("Thank you");
+        
+        
+    }
+}
---
+++ operators/AssignmentOperators.java
@@ -0,0 +1,24 @@
+public class AssignmentOperators{
+    public static void main (String [] args ){
+        int x = 10;
+
+        System.out.println("Initial value :" + " " + x);
+        x += 5;
+        System.out.println("  x += 5" +"  " + x);
+
+        x -= 5;
+        System.out.println("  x -= 5" +"  " + x);
+
+        x *= 5;
+        System.out.println("  x *= 5"+  "  " + x);
+
+        x /= 5;
+        System.out.println("  x /= 5" +"  " + x);
+
+        x %= 5;
+        System.out.println("  x %= 5" +"  " + x);
+
+
+
+    }
+}
