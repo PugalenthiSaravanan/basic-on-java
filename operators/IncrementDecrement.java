aef8df46669dd4b82fdc2ece4ea28f9324af5665
pugalpugal123 <pugalenthisaravanan70@gmail.com>
2026-08-14 07:17:17 +0530

    Added new code files

---
+++ operators/IncrementDecrement.java
@@ -0,0 +1,14 @@
+public class IncrementDecrement{
+    public static void main (String [] args){
+        int a = 5;
+        System.out.println("\n ===Increment / Decrement===");
+        System.out.println("Original Value :" + " " + a );
+        a++;
+        System.out.println("After Increment :"+ " " + a);
+
+        a--;
+        System.out.println("After Decrement :"+" " +a);
+
+    
+    }
+}
---
+++ operators/LogicalOperators.java
@@ -0,0 +1,14 @@
+public class LogicalOperators{
+    public static void main (String [] args){
+        System.out.println("\n=== Logical Operators === ");
+        boolean A = true;
+        boolean B = false;
+
+        System.out.println("And(&&) =" + " " + (A&&B));
+        System.out.println("OR (||)" +" "+ (A||B));
+        System.out.println("NOT(!)"+ " " + (!B));
+
+
+
+    }
+}
