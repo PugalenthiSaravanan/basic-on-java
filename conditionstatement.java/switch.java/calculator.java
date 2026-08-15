db8fff7876c812ffe3b76c506fe90dc45e81b2af
pugalpugal123 <pugalenthisaravanan70@gmail.com>
2026-08-15 08:51:26 +0530

    Added new code files

---
+++ conditionstatement.java/switch.java/calculator.java
@@ -0,0 +1,24 @@
+public class calculator{
+    public static void main (String [] args){
+        int a =10;
+        int b =20;
+        char operator ='-';
+        switch(operator){
+            
+        case '+':
+            System.out.println("Addition =" + (a+b));
+            break;
+        case '-':
+            System.out.println("Subtraction =" +(a-b));
+            break;
+        case '*':
+            System.out.println("Divition ="+ (a*b));
+            break;
+        case '/':
+            System.out.println("Modulus =" + (a%b));
+            break;
+        default:
+            System.out.println("Invalid operator");
+       }
+    }
+}
